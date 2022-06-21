0ad2ba1ca0cd9cbee0098f773445eda226a109ff
brian corker <bcorker123@gmail.com>
2022-06-21 12:28:57 -0400

    Updated readme, seeds, and controller

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # Merdle-Phase-3-Project
+
+Requirements:
+
+1. Use Active Record to interact with a database. ✅
+2. Have at least two models with a one-to-many relationship. ✅
+3. Set up API routes to create and read actions for both models
+4. Set up API routes for full CRUD capability for one of the models
+5. Build a separate React frontend application that interacts with the API to perform CRUD actions.
+6. Use good OO design patterns. You should have separate classes for each of your models, and create instance and class methods as necessary.
--- ruby-sinatra-backend/app/controllers/application_controller.rb
+++ ruby-sinatra-backend/app/controllers/application_controller.rb
@@ -1,9 +1,40 @@
+require 'pry'
+
 class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
   # Add your routes here
   get "/" do
     { merdles: Merdle.all, users: User.all, scores: Score.all }.to_json
+  end
+
+  get '/merdles' do
+    Merdle.all.to_json
+  end 
+
+  get '/merdles/:id' do
+    id = params[:id].to_i - 1
+    Merdle.all[id].to_json
+  end
+
+  get '/users' do
+    User.all.to_json
+  end
+
+  get '/users/:id' do
+    id = params[:id].to_i - 1
+    user = User.all[id]
+    #binding.pry
+    User.all[id].to_json
+  end
+
+  get '/scores' do
+    Score.all.to_json
+  end
+
+  get '/scores/:id' do
+    id = params[:id].to_i - 1
+    Score.all[id].to_json
   end
 
 end
