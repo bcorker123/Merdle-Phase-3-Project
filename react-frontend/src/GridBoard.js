37eb23162c8252acb4027bc5622cf164743f2759
brian corker <bcorker123@gmail.com>
2022-06-22 11:48:10 -0400

    Meme image is behind grid, and squares are black

--- react-frontend/src/GridBoard.js
+++ react-frontend/src/GridBoard.js
@@ -1,34 +1,42 @@
-import React from 'react'
-import GridSquare from './GridSquare'
+import React from "react";
+import GridSquare from "./GridSquare";
+import MemeCard from "./MemeCard";
 
 function GridBoard() {
-  const rows = []
+  const rows = [];
 
   for (let row = 0; row < 4; row++) {
-    const columns = []
+    const columns = [];
 
     for (let col = 0; col < 4; col++) {
-      columns.push(<GridSquare key={`${col}${row}`} />)
+      columns.push(<GridSquare key={`${col}${row}`} />);
     }
 
-    rows.push(columns)
+    rows.push(columns);
   }
 
   return (
     <div
       id="board-pos"
       className="container justify-content-center align-items-center"
+      style={{
+        backgroundImage: 'url("https://i.imgflip.com/1ur9b0.jpg")',
+        backgroundSize: "cover",
+        backgroundPosition: "top",
+        backgroundClip: "content-box",
+      }}
     >
       <div className="grid-board">
         {rows.map((row) => (
           <div>{row}</div>
         ))}
+        <MemeCard />
       </div>
       {/* <div className="grid-board">{grid2}</div> */}
       {/* <div className="grid-board">{rows}</div>
       <div className="grid-board">{rows}</div> */}
     </div>
-  )
+  );
 }
 
-export default GridBoard
+export default GridBoard;
--- react-frontend/src/index.js
+++ react-frontend/src/index.js
@@ -1,27 +1,28 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
-import App from './App'
-import reportWebVitals from './reportWebVitals'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import MemeCard from './MemeCard'
-import Home from './Home'
-import ScoreCard from './ScoreCard'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import MemeCard from "./MemeCard";
+import Home from "./Home";
+import ScoreCard from "./ScoreCard";
+import GridBoard from "./GridBoard";
 
-const root = ReactDOM.createRoot(document.getElementById('root'))
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<App />}>
         <Route index element={<Home />} />
-        <Route path="/play" element={<MemeCard />} />
+        <Route path="/play" element={<GridBoard />} />
         <Route path="/scores" element={<ScoreCard />} />
       </Route>
     </Routes>
   </BrowserRouter>
-)
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals()
+reportWebVitals();
