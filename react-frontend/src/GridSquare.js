db519571f4e1776ce38079fde010b515dc60040e
Abram Shook <ajshook77@gmail.com>
2022-06-22 09:14:27 -0500

    grid-work

---
+++ react-frontend/src/GridSquare.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+function GridSquare() {
+  return <div className="grid-square" />
+}
+
+export default GridSquare
--- react-frontend/src/NavHeader.js
+++ react-frontend/src/NavHeader.js
@@ -1,31 +1,26 @@
-import React from "react";
-import { Nav, Navbar } from "react-bootstrap";
-import { Link } from "react-router-dom";
+import React from 'react'
+import { Nav, Navbar } from 'react-bootstrap'
+import { Link } from 'react-router-dom'
 
 function NavHeader() {
   return (
     <div className="navigation">
       <Navbar bg="bbcolors" variant="dark" fixed="top" className="navbar py-4">
-        <Navbar.Brand className="nav-link">Meme-Mentality</Navbar.Brand>
+        <Navbar.Brand className="nav-link">Merdle</Navbar.Brand>
 
-        {/* <Nav>
-          <Nav className="nav-link">Home</Nav>
-          <Nav>
-            <Link className="nav-link" to="play">
-              Play
-            </Link>
-          </Nav>
-        </Nav> */}
         <Nav>
           <Link className="nav-link" to="/">
             Home
           </Link>
           <Link className="nav-link" to="/play">
-            Daily Discovery
+            Play
+          </Link>
+          <Link className="nav-link" to="/scores">
+            Scores
           </Link>
         </Nav>
       </Navbar>
     </div>
-  );
+  )
 }
-export default NavHeader;
+export default NavHeader
