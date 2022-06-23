a1b411002fc1bc0d11757205b02fa4e6e992451f
brian corker <bcorker123@gmail.com>
2022-06-23 11:55:55 -0400

    added points card that subtracts by 100 for each square clicked, and reveal+new meme buttons reset points and covered squares

--- react-frontend/src/GridBoard.js
+++ react-frontend/src/GridBoard.js
@@ -1,52 +1,80 @@
-import React, { useState, useEffect } from 'react'
-import { Button } from 'react-bootstrap'
-import GridSquare from './GridSquare'
+import React, { useState, useEffect } from "react";
+import { Button, Card } from "react-bootstrap";
+import GridSquare from "./GridSquare";
 
-function GridBoard() {
-  const [memes, setMemes] = useState([])
-  const [isClicked, setIsClicked] = useState(false)
-  const [revealed, setRevealed] = useState(false)
-  const [memeDisplay, setMemeDisplay] = useState('')
-  const [answer, setAnswer] = useState('')
-  const [userInput, setUserInput] = useState('')
+function GridBoard({ currentUser }) {
+  const [memes, setMemes] = useState([]);
+  const [revealed, setRevealed] = useState(false);
+  const [userInput, setUserInput] = useState("");
+  const [points, setPoints] = useState(1600);
 
   useEffect(() => {
-    fetch('http://localhost:9292/merdles/random')
+    fetch("http://localhost:9292/merdles/random")
       .then((r) => r.json())
-      .then((data) => setMemes(data))
-  }, [])
+      .then((data) => {
+        setMemes(data);
+        setPoints(1600);
+      });
+  }, []);
 
   function showAll() {
-    setRevealed(true)
+    setRevealed(true);
   }
 
   function getMeme() {
-    setIsClicked(true)
-
-    setMemeDisplay(memes.image_url)
-    setAnswer(memes.name)
+    fetch("http://localhost:9292/merdles/random")
+      .then((r) => r.json())
+      .then((data) => {
+        setMemes(data);
+      });
+    setPoints(1600);
+    pointsBG = "info";
+    setRevealed(false);
   }
 
   function handleAnswer(e) {
-    setUserInput(e.target.value)
+    setUserInput(e.target.value);
+    console.log(memes.name);
   }
 
   function handleSubmit(e) {
-    e.preventDefault()
-    console.log(answer)
+    e.preventDefault();
+    console.log();
   }
 
-  const rows = []
+  function minusPoints() {
+    setPoints(points - 100);
+  }
+
+  let pointsBG = "info";
+
+  function loseGame() {
+    pointsBG = "danger";
+  }
+
+  if (points === 0) {
+    loseGame();
+  }
+
+  const rows = [];
 
   for (let row = 0; row < 4; row++) {
-    const columns = []
+    const columns = [];
 
     for (let col = 0; col < 4; col++) {
-      columns.push(<GridSquare key={`${col}${row}`} revealed={revealed} />)
+      columns.push(
+        <GridSquare
+          key={`${col}${row}`}
+          revealed={revealed}
+          minusPoints={minusPoints}
+        />
+      );
     }
 
-    rows.push(columns)
+    rows.push(columns);
   }
+
+  console.log("rows: ", rows);
 
   return (
     <>
@@ -55,22 +83,33 @@ function GridBoard() {
           id="board-pos"
           className="container justify-content-center align-items-center"
           style={{
-            backgroundImage: isClicked ? `url(${memeDisplay})` : '',
-            backgroundSize: 'cover',
-            backgroundPosition: 'top',
-            backgroundClip: 'content-box',
+            backgroundImage: `url(${memes.image_url})`,
+            backgroundSize: "cover",
+            backgroundPosition: "top",
+            backgroundClip: "content-box",
           }}
         >
           <div className="grid-board">
             {rows.map((row) => (
-              <div>{row}</div>
+              <div key={row.id}>{row}</div>
             ))}
+            <Card bg={pointsBG} border="warning">
+              <Card.Header>Points</Card.Header>
+              <Card.Body>
+                <Card.Title>{points}</Card.Title>
+              </Card.Body>
+              <Card.Footer>
+                User: {currentUser ? currentUser : "anon"}
+              </Card.Footer>
+            </Card>
           </div>
         </div>
       </div>
+      <div style={{ backgroundColor: "white" }}>First click Reveal:</div>
       <button className="reveal" onClick={showAll}>
         REVEAL
       </button>
+      <div style={{ backgroundColor: "white" }}>Then click New Meme:</div>
       <div id="outer">
         <div className="button-slide slide-left" onClick={getMeme}>
           New Meme
@@ -84,10 +123,10 @@ function GridBoard() {
         </Button>
       </form>
     </>
-  )
+  );
 }
 
-export default GridBoard
+export default GridBoard;
 
 // {isClicked ? }
 
